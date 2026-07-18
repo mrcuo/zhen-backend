0ddd6c389e475c91889592bf6260a6541c99fe4c
CUO studio <chuengzd@gmail.com>
2026-07-18 08:47:33 +0800

    debug KV

--- api/status/[deviceId].js
+++ api/status/[deviceId].js
@@ -23,7 +23,7 @@ module.exports = async function handler(req, res) {
     }
 
     if (!process.env.KV_REST_API_URL) {
-      return res.status(200).json({ tier: 'free' }); // No KV configured, default to free
+      return res.status(200).json({ tier: 'free', debug: 'NO_KV_ENV_FOUND' });
     }
 
     const device = await kv.get(`device:${deviceId}`);
